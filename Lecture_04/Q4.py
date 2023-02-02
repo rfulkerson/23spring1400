d14e0925c669db748076b108f65bec313883d996
Robert Fulkerson <rfulkerson@unomaha.edu>
2023-02-02 16:09:39 -0600

    Adding Lecture 04

---
+++ Lecture_04/Q4.py
@@ -0,0 +1,5 @@
+val1 = int(input())
+val2 = int(input())
+print(val1, '-', val2, '=', val1 - val2)
+
+print(f"{val1} - {val2} = {val1-val2}")
---
+++ Lecture_04/Q7.py
@@ -0,0 +1,7 @@
+value = 2.71828
+
+print(f'{value:.2f}') 
+print(f'{value.2f}') 
+print(f'{value:2f}') 
+print(f'{value.:2f}') 
+
