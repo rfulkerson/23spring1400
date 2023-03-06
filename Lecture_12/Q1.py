e6beec94c52495e9967f1062a0c5247cc257110e
Robert A. Fulkerson <rfulkerson@unomaha.edu>
2023-03-06 10:00:00 -0600

    Adding Lecture 12.

---
+++ Lecture_12/Q1.py
@@ -0,0 +1,10 @@
+# How many times will Wonderful be printed after the code is executed?
+
+x = 0
+while x <= 3:
+    print("Wonderful")
+    x += 1
+while x > 1:
+    print("Wonderful")
+    x -= 1
+
